575fb42170c37f2fe0b1ac76df924010497a4754
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-07-31 21:30:14 -0300

    New interface, for revisions

--- addons/softguard/softguard.py
+++ addons/softguard/softguard.py
@@ -33,9 +33,43 @@ class softguard_import_line(osv.osv):
 
     # cue_cIdExtendido.
 
+    def _get_related_ids(self, cr, uid, ids, field_name, arg, context):
+        par_obj = self.pool.get('res.partner')
+        con_obj = self.pool.get('account.analytic.account')
+        r = {}
+        
+        for line in self.browse(cr, uid, ids, context=context):
+            par_key = line.cue_cIdExtendido.strip()
+            par_ids = par_obj.search(cr, uid, [ '|', '|',
+                ('ref','=','%s' % par_key),
+                ('ref','=','%s.0' % par_key),
+                ('category_id','=','E-%s' % par_key),
+            ])
+            con_ids = con_obj.search(cr, uid, [('name','=',line.cue_ncuenta)])
+            msg = "%s, %s" % (
+                (len(par_ids) == 0 and 'No Partner') or
+                (len(par_ids) >  1 and 'Error: Multiple Partners') or
+                (len(par_ids) == 1 and 'Partner Ok'),
+                (len(con_ids) == 0 and 'No Contract') or
+                (len(con_ids) >  1 and 'Error: Multiple Contract') or
+                (len(con_ids) == 1 and 'Contract Ok'),
+            )
+            r[line.id] = {
+                'rel_partner_ids': par_ids,
+                'rel_contract_ids': con_ids,
+                'message': msg,
+            }
+
+        return r
+
     _columns = {
-        'import_id': fields.many2one('softguard.import', 'Softguard Import'),
-        'RowNumber': fields.integer('Row number'),
+        'import_id': fields.many2one('softguard.import', 'Softguard Import', ondelete="cascade", required=True),
+        'rel_partner_ids': fields.function(_get_related_ids, type='many2many', obj='res.partner', string='Related Partners', method=True, multi='related'),
+        'rel_contract_ids': fields.function(_get_related_ids, type='many2many', obj='account.analytic.account', string='Related Contracts', method=True, multi='related'),
+        'message': fields.function(_get_related_ids, type='char', string='Message', multi='related'),
+        'state': fields.selection([('draft', 'Draft'),('hold','Hold'),('open','Open'),('done','Done')], 'State'),
+
+        'name': fields.char('Row number'),
         'cue_iid': fields.integer('cue_iid'),
         'cue_clinea': fields.char('cue_clinea'),
         'cue_ncuenta': fields.char('cue_ncuenta'),
@@ -70,19 +104,43 @@ class softguard_import_line(osv.osv):
         'cue_nparticion': fields.integer('CUE Partition Number'),
     }
 
-    def do_import(self, cr, uid, ids, context=None):
+    _defaults = {
+        'state': 'open',
+    }
+
+    def do_draft(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {'state':'draft'})
+        return True
+    
+    def do_open(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {'state':'open'})
+        return True
+
+    def do_hold(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {'state':'hold'})
+        return True
+
+    def do_done(self, cr, uid, ids, context=None):
         par_obj = self.pool.get('res.partner')
         cou_obj = self.pool.get('res.country')
         sta_obj = self.pool.get('res.country.state')
         con_obj = self.pool.get('account.analytic.account')
-        
-        for line in self.browse(cr, uid, ids, context=context):
-            par_key = line.cue_cIdExtendido.strip()
-            par_ids = par_obj.search(cr, uid, [ '|', '|',
-                ('ref','=','%s' % par_key),
-                ('ref','=','%s.0' % par_key),
-                ('category_id','=','E-%s' % par_key),
-            ])
+
+        hold_ids = []
+        done_ids = []
+
+        for line in self.browse(cr, uid, ids):
+            pars = line.rel_partner_ids
+            cons = line.rel_contract_ids
+
+            if line.state != 'open':
+                continue
+
+            if len(pars) > 1 or len(cons) > 1:
+                hold_ids.append(line.id)
+                continue
+
+            par_ids = [ p.id for p in  pars ]
 
             values = {
                 'ref': line.cue_ncuenta,
@@ -95,13 +153,7 @@ class softguard_import_line(osv.osv):
                 'zip': line.cue_ccodigopostal,
             }
 
-            # If more than one partner for this reference, we send a message to advise.
-            if len(par_ids) > 1:
-                msg =  _('<b>Processing row %i, we found duplicated partner. Please check which partner must be deleted</b>') % line.RowNumber
-                par_obj.message_post(cr, uid, par_ids, body=msg, context=context)
-                continue
-
-            elif len(par_ids) == 1:
+            if len(par_ids) == 1:
                 keep_values = par_obj.read(cr, uid, par_ids, [
                     'name',
                     'street',
@@ -114,16 +166,15 @@ class softguard_import_line(osv.osv):
                 values = update(values, keep_values)
                 del values['id']
                 par_obj.write(cr, uid, par_ids, values)
-                msg =  _('<b>Updated from row %i using softguard import %s</b>') % (line.RowNumber, line.import_id.name)
+                msg =  _('<b>Updated from row %s using softguard import %s</b>') % (line.name, line.import_id.name)
                 par_obj.message_post(cr, uid, par_ids, body=msg, context=context)
 
             else:
                 par_ids = [ par_obj.create(cr, uid, values) ]
-                msg =  _('<b>Created from row %i using softguard import %s</b>') % (line.RowNumber, line.import_id.name)
+                msg =  _('<b>Created from row %s using softguard import %s</b>') % (line.name, line.import_id.name)
                 par_obj.message_post(cr, uid, par_ids, body=msg, context=context)
 
-            # Update and create contract.
-            con_ids = con_obj.search(cr, uid, [('name','=',line.cue_ncuenta)])
+            con_ids = [ c.id for c in  cons ]
 
             values = {
                 'name': line.cue_ncuenta,
@@ -134,33 +185,51 @@ class softguard_import_line(osv.osv):
                 'partner_shipping_id': par_ids[0],
             }
 
-            if len(con_ids) > 1:
-                msg =  _('<b>Processing row %i, we found duplicated contract. Please check which contract must be deleted</b>') % line.RowNumber
-                con_obj.message_post(cr, uid, con_ids, body=msg, context=context)
-                continue
-
-            elif len(con_ids) == 1:
-                keep_values = con_obj.read(cr, uid, par_ids, [
+            if len(con_ids) == 1:
+                keep_values = con_obj.read(cr, uid, con_ids, [
                     'state',
                     'partner_id',
                     'partner_invoice_id',
                     'partner_shipping_id',
-                ])
+                ])[0]
                 values = update(values, keep_values)
                 del values['id']
                 con_obj.write(cr, uid, con_ids, values)
-                msg =  _('<b>Updated from row %i using softguard import %s</b>') % (line.RowNumber, line.import_id.name)
+                msg =  _('<b>Updated from row %s using softguard import %s</b>') % (line.name, line.import_id.name)
                 con_obj.message_post(cr, uid, con_ids, body=msg, context=context)
 
             else:
                 con_ids = [ con_obj.create(cr, uid, values) ]
-                msg =  _('<b>Created from row %i using softguard import %s</b>') % (line.RowNumber, line.import_id.name)
+                msg =  _('<b>Created from row %s using softguard import %s</b>') % (line.name, line.import_id.name)
                 con_obj.message_post(cr, uid, con_ids, body=msg, context=context)
 
-            #if par_ids and con_ids:
-           #     pass
+            done_ids.append(line.id)
 
-        pass
+        self.write(cr, uid, hold_ids, {'state':'hold'})
+        self.write(cr, uid, done_ids, {'state':'done'})
+        return True
+
+    def open_partners(self, cr, uid, ids, context=None):
+        line = self.browse(cr, uid, ids)[0]
+        return  {
+            'domain':[('id','in',[ p.id for p in line.rel_partner_ids ])],
+            'name':_('Partners'),
+            'view_type':'form',
+            'view_mode':'tree,form',
+            'res_model':'res.partner',
+            'type':'ir.actions.act_window',
+        }
+
+    def open_contracts(self, cr, uid, ids, context=None):
+        line = self.browse(cr, uid, ids)[0]
+        return  {
+            'domain':[('id','in',[ p.id for p in line.rel_contract_ids ])],
+            'name':_('Partners'),
+            'view_type':'form',
+            'view_mode':'tree,form',
+            'res_model':'account.analytic.account',
+            'type':'ir.actions.act_window',
+        }
 
 softguard_import_line()
 
@@ -176,11 +245,14 @@ class softguard_import(osv.osv):
     }
 
     def do_load(self, cr, uid, ids, context=None):
+        def _update_(d):
+            d['name'] = d.pop('RowNumber')
+            return d
         for imp in self.browse(cr, uid, ids, context=context):
             if imp.line_ids:
                 continue
             data = StringIO.StringIO(base64.decodestring(imp.data))
-            import_lines = [ (0,0,values) for values in csv.DictReader(data) ]
+            import_lines = [ (0,0,_update_(values)) for values in csv.DictReader(data) ]
             self.write(cr, uid, imp.id, { 'line_ids': import_lines })
         return True
 
@@ -188,8 +260,39 @@ class softguard_import(osv.osv):
         implin_obj = self.pool.get('softguard.import.line')
         for imp in self.browse(cr, uid, ids, context=context):
             if imp.line_ids:
-                implin_obj.do_import(cr, uid, [line.id for line in imp.line_ids], context=context)
-        return True
+                implin_obj.do_done(cr, uid, [line.id for line in imp.line_ids], context=context)
+        return  {
+            'domain':[('state','=','hold')],
+            'name':_('Hold Lines'),
+            'view_type':'form',
+            'view_mode':'tree,form',
+            'res_model':'softguard.import.line',
+            'type':'ir.actions.act_window',
+        }
+
+    def do_list_all(self, cr, uid, ids, context=None):
+        return  {
+            'domain':[('import_id','in',ids)],
+            'name':_('Lines'),
+            'view_type':'form',
+            'view_mode':'tree,form',
+            'res_model':'softguard.import.line',
+            'type':'ir.actions.act_window',
+        }
+
+    def do_list_hold(self, cr, uid, ids, context=None):
+        return  {
+            'domain':[('state','=','hold'),('import_id','in',ids)],
+            'name':_('Hold Lines'),
+            'view_type':'form',
+            'view_mode':'tree,form',
+            'res_model':'softguard.import.line',
+            'type':'ir.actions.act_window',
+        }
+
+
+
+
 
 softguard_import()
 
